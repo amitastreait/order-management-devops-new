fa732519cdc7fd701e82f992c677c9c93f268b86
Amit Singh <cse.amitallenhouse@gmail.com>
2026-03-12 20:20:26 +0530

    Update checkout.js

--- checkout/checkout.js
+++ checkout/checkout.js
@@ -1,8 +1,4 @@
 function checkout(){
     /** sample checkout code */
     /** sample checkout code */
-    /** sample checkout code */
-    /** sample checkout code */
-    /** sample checkout code */
-    /** sample checkout code */
 }
